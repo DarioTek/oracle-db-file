2ad7102b0f61ce806cfd4b29e6cf047d17234673
DarioTek <dmontecastro@yahoo.com>
2020-07-12 16:00:07 -0400

    DarioTek
    Updated

---
+++ YAHOO_FINANCE_QUOTE_SUMMARY_VIEW.sql
@@ -0,0 +1,34 @@
+/*
+ * trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW
+ */
+ CREATE OR REPLACE VIEW trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW AS
+    SELECT
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.record_datetime,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.symbol,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.live_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.previous_closing_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.opening_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.bid_offer_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.bid_offer_quantity,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.asking_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.asking_quantity,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.days_range_low_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.days_range_high_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.fifty_two_week_range_low_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.fifty_two_week_range_high_price,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.volume,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.average_volume,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.market_capitalization,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.beta,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.pe_ratio_ttm,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.eps_ttm,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.earnings_date,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.earnings_date_end,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.dividend,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.dividend_yield,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.ex_dividend_date,
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.one_year_target_price
+    FROM
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY 
+    WHERE
+        trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY.record_datetime in (SELECT max(record_datetime) FROM trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY);
--- stock_quote_summary_analysis.sql
+++ stock_quote_summary_analysis.sql
@@ -1,4 +1,13 @@
 /*
+ * Validation queries
+ */
+-- Live price is null
+select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW where live_price is null
+
+-- ONE_YEAR_TARGET_PRICE is null
+select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW where one_year_target_price is null
+ 
+/*
  * Which stock has the most earning potential for the year?
  */ 
 SELECT *
@@ -21,7 +30,36 @@ select RECORD_DATETIME, count(*) from trading_investing.YAHOO_FINANCE_QUOTE_SUMM
 /*
  * Which stock has the most dividend yield
  */ 
+-- Which stock has the most dividend yield throughout all the records scrapped from Yahoo Finance
 select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY where DIVIDEND_YIELD in (select max(dividend_yield) from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY)
 
+-- Which stock has the most dividend yield (most current data)
+select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW where DIVIDEND_YIELD in (select max(dividend_yield) from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW)
 
-select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW
+-- Which stock has the most dividend yield (most current data) top 10
+select * from (select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW order by DIVIDEND_YIELD desc) where rownum <= 10
+
+
+/*
+ * Which stock has the most dividend Beta
+ */ 
+-- Which stock has the highest (most current data)
+select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW where BETA in (select max(BETA) from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW)
+
+-- Which stock has the highest (most current data) top 10
+select * from (select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW order by BETA desc) where rownum <= 10
+
+/*
+ * Which stock has the highest one year potential gain
+ */
+
+-- Which stock has the highest one year potential gain
+select * from  trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW where ONE_YEAR_POTENTIAL_GAIN in (select max(ONE_YEAR_POTENTIAL_GAIN) from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW)
+-- ANSWER: AZO (AutoZone) on 7/12/2020
+
+-- Which stock has the highest one year potential gain (most current data) top 10
+select * from (select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW order by ONE_YEAR_POTENTIAL_GAIN desc) where rownum <= 10 and ONE_YEAR_POTENTIAL_GAIN is not NULL
+
+-- Which stock has the highest one year potential gain percentage(most current data) top 10
+select * from (select * from trading_investing.YAHOO_FINANCE_QUOTE_SUMMARY_VIEW order by ONE_YEAR_POTENTIAL_GAIN_PERCENTAGE desc) where rownum <= 10 and ONE_YEAR_POTENTIAL_GAIN_PERCENTAGE is not NULL
+
